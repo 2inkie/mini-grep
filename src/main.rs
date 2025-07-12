b0738d28bbd5262682be0958194f88b5b03380db
Antoan Matein <antoandmatein@gmail.com>
2025-07-12 13:41:38 +0300

    Added case sensitivity handling.

--- src/main.rs
+++ src/main.rs
@@ -4,38 +4,56 @@ use std::error::Error;
 struct Config {
     query: String,
     filename: String,
+    case_sensitive: bool,
 }
 
 impl Config {
     fn build(args: &[String]) -> Result<Config, &'static str> {
-        if args.len() != 3 {
+        if args.len() != 4 {
             return Err("Not enough arguments - usage: mini_grep <query> <filename>");
         }
         let query = args[1].clone();
         let filename = args[2].clone();
-        Ok(Config { query, filename })
+        let case_sensitive = env::var("CASE_INSENSITIVE").is_err();
+        Ok(Config{
+            query,
+            filename,
+            case_sensitive
+        })
     }
+}
+
+fn search_case_sensitive<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
+    contents
+        .lines()
+        .filter(|line| line.contains(query))
+        .collect()
+}
+
+fn search_case_insensitive<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
+    let query = query.to_lowercase();
+    contents
+        .lines()
+        .filter(|line| line.to_lowercase().contains(&query))
+        .collect()
 }
 
 fn run(config: Config) -> Result<(), Box<dyn Error>> {
-    println!("Searching for '{}' in file '{}'", config.query, config.filename);
+    let contents = fs::read_to_string(&config.filename)?;
 
-    let filepath = "./files/".to_string();
-    let file = filepath + &config.filename;
+    let results = if config.case_sensitive {
+        search_case_sensitive(&config.query, &contents)
+    } else {
+        search_case_insensitive(&config.query, &contents)
+    };
 
-    let contents = fs::read_to_string(file)?;
+    for line in results {
+        println!("{}", line);
+    }
 
-    search_in_file(&config.query, &contents);
     Ok(())
 }
 
-fn search_in_file(query: &str, file_contents: &str) {
-    for line in file_contents.lines() {
-        if line.contains(query) {
-            println!("Found: {}", line);
-        }
-    }
-}
 fn main() {
     let args: Vec<String> = env::args().collect();
 
